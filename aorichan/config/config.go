6671da00e52f3eea8febd9d4c32aaa3c21ada02b
sayyeah-t <sayyeah.take@gmail.com>
2018-03-09 00:52:03 +0900

    base config and additional config

---
+++ aorichan/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+    "github.com/sayyeah-t/take2systembot/base/config"
+)
+
+type AoriConfig struct {
+    baseconfig.BaseConfig
+    IksmChannel string
+    WorkChannels []string
+    Commands []string
+}
+
+
+func (c *AoriConfig) AdditionalInit() bool {
+    println("override!")
+    section, err := c.Cfg.GetSection("splatoon")
+    if err != nil {
+        print("cannot read section splatoon")
+        return false
+    }
+    iksmchannel, err := section.GetKey("iksmchannel")
+    if err != nil {
+        println(err.Error())
+        return false
+    }
+    c.IksmChannel = iksmchannel.MustString("none")
+
+    workchannels, err := section.GetKey("workchannels")
+    if err != nil {
+        println(err.Error())
+        return false
+    }
+    c.WorkChannels = workchannels.Strings(",")
+
+    commands, err := section.GetKey("commands")
+    if err != nil {
+        println(err.Error())
+        return false
+    }
+    c.Commands = commands.Strings(",")
+    
+    return true
+}
--- base/config/config.go
+++ base/config/config.go
@@ -1,33 +1,60 @@
-package config
+package baseconfig
 
 import (
     "github.com/go-ini/ini"
 )
 
-var path = "C:/Users/Seiya/workspace/go/src/github.com/sayyeah-t/take2systembot/etc/take2systembot/"
+var path = "C:/Program Files/Git/etc/take2systembot/"
 
-type BotConfig struct {
-    var IsLoaded bool
-    var Channel string
-    var EnabledCommands []string
+type BaseConfig struct {
+    IsLoaded bool
+    Token string
+    //Channel string
+    //EnabledCommands []string
+    Cfg *ini.File
 }
 
-func (c BotConfig) Init() {
+func (c *BaseConfig) Init(filename string) {
     c.IsLoaded = false
     cfg, err := ini.InsensitiveLoad(path + filename)
     if err != nil {
         println(err.Error())
         return
     }
+    c.Cfg = cfg
+    println("Path: " + path + filename)
+    section, err := cfg.GetSection("default")
+    if err != nil {
+        println(err.Error())
+        return
+    }
 
+    // load default attributes
+    token, err := section.GetKey("token")
+    if err != nil {
+        println(err.Error())
+        return
+    }
+    c.Token = token.MustString("none")
+    c.IsLoaded = true
 }
 
-func GetConfig(filename string) *ini.Section {
-    println("Path: " + path + filename)
-    sec1, err := cfg.GetSection("default")
+
+func (c *BaseConfig) AdditionalInit() bool {
+    println("You can override this function")
+    return true
+}
+/*
+func (c BaseConfig) GetConfig(secName string) *ini.Section {
+    if c.IsLoaded {
+        println("Config file unloaded")
+        return nil
+    }
+    sec, err := c.GetSection(secName)
     if err != nil {
         println(err.Error())
         return nil
     }
-    return sec1
+    return sec
 }
+*/
